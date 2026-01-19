1e5f8162b071ce80bea6ecdc167a33905863a2ce
Denis Carriere <carriere.denis@gmail.com>
2026-01-19 17:35:40 -0500

    clean up fills

--- clickhouse/schema.1.fills.sql
+++ clickhouse/schema.1.fills.sql
@@ -19,10 +19,6 @@ ALTER TABLE fills
     ADD COLUMN IF NOT EXISTS fee_token                   String,
     ADD COLUMN IF NOT EXISTS twap_id                     UInt64 COMMENT 'Time-Weighted Average Price Identifier',
     ADD COLUMN IF NOT EXISTS client_order_id             String,
-    -- liquidation fields (optional) --
-    ADD COLUMN IF NOT EXISTS liquidated_user             String,
-    ADD COLUMN IF NOT EXISTS mark_px                     String,
-    ADD COLUMN IF NOT EXISTS liquidation_method          String,
 
     -- PROJECTIONS for analytics (minute & count) --
     ADD PROJECTION IF NOT EXISTS prj_coin_count ( SELECT coin, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY coin ),
@@ -42,21 +38,11 @@ ALTER TABLE fills
 -- Only contains fills that have liquidation data
 CREATE TABLE IF NOT EXISTS fills_liquidation AS fills;
 ALTER TABLE fills_liquidation
-    -- Modify liquidation field types --
-    MODIFY COLUMN IF EXISTS mark_px Float64 COMMENT 'Mark price for liquidation',
+    -- liquidation field --
+    ADD COLUMN IF NOT EXISTS liquidated_user             String,
+    ADD COLUMN IF NOT EXISTS mark_px                     Float64 COMMENT 'Mark price for liquidation',
+    ADD COLUMN IF NOT EXISTS liquidation_method          String,
 
     -- PROJECTIONS for analytics (minute & count) --
-    ADD PROJECTION IF NOT EXISTS prj_coin_count ( SELECT coin, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY coin ),
-    ADD PROJECTION IF NOT EXISTS prj_user_count ( SELECT user, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY user ),
     ADD PROJECTION IF NOT EXISTS prj_liquidated_user_count ( SELECT liquidated_user, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY liquidated_user ),
-    ADD PROJECTION IF NOT EXISTS prj_side_count ( SELECT side, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY side ),
-    ADD PROJECTION IF NOT EXISTS prj_direction_count ( SELECT direction, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY direction ),
-    ADD PROJECTION IF NOT EXISTS prj_liquidation_method_count ( SELECT liquidation_method, count(), min(block_num), max(block_num), min(timestamp), max(timestamp), min(minute), max(minute) GROUP BY liquidation_method ),
-
-    -- minute projections --
-    ADD PROJECTION IF NOT EXISTS prj_coin_by_minute ( SELECT coin, minute, count() GROUP BY coin, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_user_by_minute ( SELECT user, minute, count() GROUP BY user, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_liquidated_user_by_minute ( SELECT liquidated_user, minute, count() GROUP BY liquidated_user, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_side_by_minute ( SELECT side, minute, count() GROUP BY side, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_direction_by_minute ( SELECT direction, minute, count() GROUP BY direction, minute ),
-    ADD PROJECTION IF NOT EXISTS prj_all_by_minute ( SELECT coin, side, direction, liquidation_method, minute, count() GROUP BY coin, side, direction, liquidation_method, minute );
+    ADD PROJECTION IF NOT EXISTS prj_liquidated_user_by_minute ( SELECT liquidated_user, minute, count() GROUP BY liquidated_user, minute );
